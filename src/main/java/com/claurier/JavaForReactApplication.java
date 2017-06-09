038d924d98fec064cdfc493f81003ce759f7f5fc
CARL <fathera6@gmail.com>
2017-06-09 19:51:01 +0200

    TEST

---
+++ src/main/java/com/claurier/JavaForReactApplication.java
@@ -0,0 +1,12 @@
+package com.claurier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaForReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaForReactApplication.class, args);
+	}
+}
